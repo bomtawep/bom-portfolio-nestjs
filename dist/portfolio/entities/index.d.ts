918b813963a224e40b3c51976327424a1ff48d2c
bomtawep <bomtawep@gmail.com>
2023-06-15 20:56:12 +0700

    init

---
+++ dist/portfolio/entities/index.d.ts
@@ -0,0 +1,2 @@
+import { Portfolio } from "./portfolio.entity";
+export declare const entities: (typeof Portfolio)[];
---
+++ dist/portfolio/entities/portfolio.entity.d.ts
@@ -0,0 +1,9 @@
+export declare class Portfolio {
+    id: number;
+    firstname: string;
+    lastname: string;
+    gender: string;
+    email: string;
+    created: Date;
+    updated: Date;
+}
